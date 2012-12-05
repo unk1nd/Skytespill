2d865bf2e7073f21400f04c292c421f6b07db185
Mikael Bendiksen <duuxman@gmail.com>
2012-12-05 13:59:32 +0100

    BigIsles class added

---
+++ Skytespill/BigIsles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Skytespill
+{
+    class BigIsles
+    {
+        private float x, y;
+        private float screenheight, screenwidth;
+
+
+        private Image isles2 = global::Skytespill.Properties.Resources.mountain_2;
+
+        public Rectangle islesArea
+        {
+            get { return new Rectangle((int)this.x, (int)this.y, isles2.Width, isles2.Height); }
+        }
+
+        public BigIsles(int screenwidth, int screenheight, int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.screenheight = screenheight;
+            this.screenwidth = screenwidth;
+        }
+
+        public void draw(System.Drawing.Graphics g)
+        {
+            g.DrawImage(isles2, x, y, isles2.Width, isles2.Height);
+        }
+    }
+}
